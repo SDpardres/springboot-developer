6704fdc081427c31159b5dc1945f5f51f74d7b37
최호연 <choihy2k@gmail.com>
2025-03-12 12:47:20 +0900

    add file : add layered file for spring boot 3 sample code
    modified file : build.gradle
    add dependency for spring boot3 sample code

---
+++ src/main/java/com/springboot/springbootdeveloper/Member.java
@@ -0,0 +1,15 @@
+package com.springboot.springbootdeveloper;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+
+}
