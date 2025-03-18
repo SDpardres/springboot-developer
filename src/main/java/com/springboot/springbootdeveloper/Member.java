3f9df3ec7e47b0c0d9946f932ced9fca1528ecd5
최호연 <choihy2k@gmail.com>
2025-03-18 12:33:45 +0900

    modified files : Hierarchical design

--- src/main/java/com/springboot/springbootdeveloper/Member.java
+++ src/main/java/com/springboot/springbootdeveloper/Member.java
@@ -1,6 +1,6 @@
 package com.springboot.springbootdeveloper;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,5 +11,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 public class Member {
+    
+    // id는 DB 테이블의 id 컬럼과 매칭
+    // name은 DB 테이블의 name 컬럼과 매칭
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", updatable = false)
+    private Long id;
 
+    @Column(name="name", nullable = false)
+    private String name;
 }
--- src/main/java/com/springboot/springbootdeveloper/TestService.java
+++ src/main/java/com/springboot/springbootdeveloper/TestService.java
@@ -1,7 +1,17 @@
 package com.springboot.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TestService {
+
+    @Autowired
+    private MemberRepository memberRepository;  //빈 주입
+
+    public List<Member> getAllMembers() {//맴버 목록 얻는 메서드
+        return memberRepository.findAll();
+    }
 }
