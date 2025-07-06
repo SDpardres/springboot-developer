a98213dd440ff496abb63d6f062b98bff853d92c
SDpardres <choihy2k@gmail.com>
2025-07-06 21:39:58 +0900

    modified file : AddArticleReq.java
    - Adding annotations to the Dto class

--- src/main/java/com/springboot/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/com/springboot/springbootdeveloper/dto/AddArticleRequest.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor //기본 생성자 추가
+@AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자 추가
 @Getter
 public class AddArticleRequest {
     
